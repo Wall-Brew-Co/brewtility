dd52db341ed7a198845f38ac7a60b129fdfb3e6e
WallBrewBot <the.wall.brew@gmail.com>
2023-02-10 15:17:28 +0000

    Bumped com.wallbrew/common-beer-data from 1.1.0 to 1.2.0.
    Inspect dependency changes here: https://github.com/Wall-Brew-Co/common-beer-data/blob/v1.2.0/CHANGELOG.md

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
   :plugins [[lein-cljsbuild "1.1.8"]]
 
   :profiles {:uberjar {:aot :all}
-             :dev     {:dependencies [[com.wallbrew/common-beer-data "1.1.0"]
+             :dev     {:dependencies [[com.wallbrew/common-beer-data "1.2.0"]
                                       [com.wallbrew/common-beer-format "2.1.0"]
                                       [doo "0.1.11"]]
                        :plugins      [[lein-doo "0.1.11"]]}}
