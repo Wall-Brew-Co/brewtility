f32bba28436f397e902124fe0ee73b9922b1ff5b
Nick Nichols <nichols1991@gmail.com>
2024-09-20 16:36:21 +0000

    Update changelog

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.wallbrew/brewtility "2.0.1"
+(defproject com.wallbrew/brewtility "2.1.0"
   :description "Utility functions for all of your brewing needs."
   :url "https://github.com/Wall-Brew-Co/brewtility"
   :license {:name         "MIT"
