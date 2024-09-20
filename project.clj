c4e866a2723bbbe3dff99ecf077590645a10cd83
Nick Nichols <nichols1991@gmail.com>
2024-09-20 16:34:33 +0000

    Install bouncer

--- project.clj
+++ project.clj
@@ -4,17 +4,20 @@
   :license {:name         "MIT"
             :url          "https://opensource.org/licenses/MIT"
             :distribution :repo
-            :comments     "Same-as all Wall-Brew projects."}
+            :comments     "Same-as all Wall-Brew projects"}
   :scm {:name "git"
         :url  "https://github.com/Wall-Brew-Co/brewtility"}
+  :pom-addition [:organization
+                 [:name "Wall Brew Co."]
+                 [:url "https://wallbrew.com"]]
   :dependencies [[com.wallbrew/spoon "1.2.3"]
                  [org.clojure/clojure "1.12.0"]
                  [org.clojure/clojurescript "1.11.132" :scope "provided"]]
 
   :plugins [[com.github.clj-kondo/lein-clj-kondo "2024.08.29"]
             [com.wallbrew/lein-sealog "1.6.0"]
-            [lein-cljsbuild "1.1.8"]
-            [lein-project-version "0.1.0"]
+            [com.wallbrew/bouncer "1.0.0"]
+            [lein-cljsbuild/lein-cljsbuild "1.1.8"]
             [mvxcvi/cljstyle "0.16.630"]
             [ns-sort "1.0.3"]]
 
